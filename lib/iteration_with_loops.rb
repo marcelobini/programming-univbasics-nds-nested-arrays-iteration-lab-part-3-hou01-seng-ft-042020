005ef7e6fc7354035c605f9f7631a7070beb6a7a
Marcelo Bini <marcelobini@gmail.com>
2020-04-03 21:11:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@
      counter2=0
       while counter2 < src[counter1].length do
         if src[counter1][counter2].class == String
-          phrase= src[counter1][counter2].join(' ')
+          phrase= src[counter1][counter2].join(" ")
         new_array << phrase
         counter2=counter2+1
        end
